5d7e5e4a2c0b08306bb7f2861693d9d5c5927c24
eddie-etg <eduardo.david357@gmail.com>
2025-07-17 15:46:07 -0400

    Add project files.

---
+++ MemoryFlipCardGame/Models/Card.cs
@@ -0,0 +1,21 @@
+﻿namespace MemoryFlipCardGame.Models
+{
+    public class Card
+    {
+        public string Id { get; set; }
+        public int Value { get; set; }
+        public bool IsFlipped { get; set; }
+        public bool IsMatched { get; set; }
+        public string FrontImage { get; set; }
+        public string BackImage { get; set; }
+
+        public Card(string id, int value, string frontImage, string backImage)
+        {
+            Id = id;
+            Value = value;
+            FrontImage = frontImage;
+            BackImage = backImage;
+        }
+    }
+    
+}
