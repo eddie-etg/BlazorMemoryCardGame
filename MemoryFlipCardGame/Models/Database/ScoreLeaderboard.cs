c38eeb828275a7c8602558d6a5ac4c0f6c389622
eddie-etg <eduardo.david357@gmail.com>
2025-07-24 10:02:31 -0400

    Added leaderboard connected to db

---
+++ MemoryFlipCardGame/Models/Database/ScoreLeaderboard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace MemoryFlipCardGame.Models.Database;
+
+[Table("ScoreLeaderboard")]
+public partial class ScoreLeaderboard
+{
+    [Key]
+    public int Id { get; set; }
+
+    [StringLength(9)]
+    public string? PlayerName { get; set; }
+
+    public int? Wins { get; set; }
+
+    public int? TotalFlips { get; set; }
+
+    public int? TotalMatches { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? DatePlayed { get; set; }
+}
